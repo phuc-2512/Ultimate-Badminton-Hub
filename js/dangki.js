f093bca290895973abea18104803cfd10b6d0d1f
phuc-2512 <160859584+phuc-2512@users.noreply.github.com>
2024-05-02 21:02:41 +0700

    main

--- js/dangki.js
+++ js/dangki.js
@@ -1,15 +1,30 @@
 const login = document.getElementById("button");
 login.addEventListener('click', checkValue);
 function checkValue(event) {
-    event.preventDefault();
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-    if (email === "") {
-        alert("email không trống");
-    } else if (password === "") {
-        alert("mật khẩu không trống")
-    } else {
-        alert("đăng nhập thành công");
-        location.href = "./trangchu.html"
-    }
+  event.preventDefault();
+  var ten = document.getElementById("text").value;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  if(text === "") {
+    alert("tên không để trống")
+  }else if (email ==="") {
+    alert("email không để trống")
+  }else if(password ==="") {
+    alert("mật khẩu không để trống")
+  }else {
+    alert("đăng kí thành công")
+    location.href = "./dangnhap.html"
+  }
+}
+
+// local storage
+
+var a = document.getElementById('text-1')
+var b = document.getElementById('email-1')
+var c = document.getElementById('password-1')
+
+function getInt() {
+  localStorage.setItem('Tên', a.value);
+  localStorage.setItem()
+  localStorage.setItem
 }
--- js/dangnhap.js
+++ js/dangnhap.js
@@ -1,25 +1,15 @@
 const login = document.getElementById("button");
 login.addEventListener('click', checkValue);
 function checkValue(event) {
-  event.preventDefault();
-  var text = document.getElementById("text").value;
-  var text = document.getElementById("number").value;
-  var email = document.getElementById("email").value;
-  var password = document.getElementById("password").value;
-  var number = document.getElementById("number-1").value;
-  if(text === "") {
-    alert("tên không để trống")
-  }else if (text ==="") {
-    alert("số điện thoại không để trống")
-  }else if (email ==="") {
-    alert("email không để trống")
-  }else if(password ==="") {
-    alert("mật khẩu không để trống")
-  }else if(number === "") {
-    alert("tuổi không để trống")
-  }else {
-    alert("đăng kí thành công")
-    location.href = "./dangnhap.html"
-  }
+    event.preventDefault();
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    if (email === "") {
+        alert("email không trống");
+    } else if (password === "") {
+        alert("mật khẩu không trống")
+    } else {
+        alert("đăng nhập thành công");
+        location.href = "./trangchu.html"
+    }
 }
- 
